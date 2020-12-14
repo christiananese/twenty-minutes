73590fd94d69768569e730b7da868ef81707a87a
Christian Anese <christian.anese@gmail.com>
2020-12-14 20:16:00 +0100

    [API] Add post service

--- api/src/modules/posts/posts.controller.ts
+++ api/src/modules/posts/posts.controller.ts
@@ -1,24 +1,28 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { PostEntity } from './entities/post';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
   @Get()
-  findAll(): string {
-    return 'Return a list of blog posts';
+  async findAll(): Promise<PostEntity[]> {
+    return this.postsService.findAll();
   }
 
   @Get(':slug')
-  findOne(@Param('slug') slug): string {
-    return `Return a blog post by given slug ${slug}`;
+  findOne(@Param('slug') slug): PostEntity {
+    return this.postsService.findOne(slug);
   }
 
   @Get(':slug/comments')
   findComments(@Param('slug') slug): string {
-    return `Return a all comments for post ${slug}`;
+    return this.postsService.findComments(slug);
   }
 
   @Post(':slug/comments')
-  createComment(@Param('slug') slug): string {
-    return `Create a new commnet for post ${slug}`;
+  createComment(@Param('slug') slug, @Body('comment') comment): string {
+    return this.postsService.createComment(slug, comment);
   }
 }
---
+++ api/src/modules/posts/posts.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CommentEntity } from './entities/comment';
+import { PostEntity } from './entities/post';
+
+/**
+ * Helper function to simulate db filter by slug
+ * @param postList the list of posts
+ * @param slug the posts slug
+ */
+const findPostBySlug = (postList, slug) => {
+  return postList.filter((post) => {
+    return post.slug === slug;
+  });
+};
+
+@Injectable()
+export class PostsService {
+  private readonly posts: PostEntity[] = [];
+
+  findAll(): PostEntity[] {
+    return this.posts;
+  }
+
+  findOne(slug: string): PostEntity {
+    // 1. find post by given slug
+    return findPostBySlug(this.posts, slug);
+  }
+
+  findComments(slug: string): string {
+    // 1. find post by given slug
+    // 2. return the posts comments
+    return `Return a all comments for post ${slug}`;
+  }
+
+  createComment(slug: string, comment: CommentEntity): string {
+    // 1. find post by given slug
+    // 2. create new comment with provided data
+    // 3. Push comment to ths posts comments
+    // 4. "persist" post
+    // 5. return post entity
+    return `Create a new commnet for post ${slug} with ${comment}`;
+  }
+}
