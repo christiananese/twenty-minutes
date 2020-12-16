7b67ab56f0bc022e88b62e45f8d28ea33caa3a5b
Christian Anese <christian.anese@gmail.com>
2020-12-16 08:45:23 +0100

    [UI] Remove create-next-app dummy styles

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,22 +1,23 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { fetchAPI } from '../lib/api';
 import PostCard, { PostProps } from '../components/PostCard';
 import { GetStaticProps } from 'next';
 
 export default function Home({ posts }: { posts: PostProps[] }) {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col justify-center items-center px-4">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>TwentyMinutes Blog</h1>
-        {posts.map((post: PostProps, index: number) => (
-          <PostCard key={index} title={post.title} excerpt={post.excerpt} featureImage={post.featureImage} />
-        ))}
+      <main className="max-w-full">
+        <h1 className="text-3xl md:text-6xl mb-8">TwentyMinutes Blog</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-8 gap-4 md:gap-6 max-w-full md:max-w-screen-lg">
+          {posts.map((post: PostProps, index: number) => (
+            <PostCard key={index} title={post.title} excerpt={post.excerpt} featureImage={post.featureImage} />
+          ))}
+        </div>
       </main>
     </div>
   );
