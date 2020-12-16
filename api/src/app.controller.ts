0306c3fe5874885d7fd683f3a68ff35f44ac4891
Christian Anese <christian.anese@gmail.com>
2020-12-16 09:28:30 +0100

    [API] Test that post-seed works as expected

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
 
   @Get()
   @ApiTags('Healthcheck')
-  getHello(): string {
+  ping(): string {
     return this.appService.ping();
   }
 }
---
+++ api/src/modules/posts/posts.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsService } from './posts.service';
+
+describe('PostsService', () => {
+  let service: PostsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PostsService],
+    }).compile();
+
+    service = module.get<PostsService>(PostsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('when initialized', () => {
+    it('should create 20 dummy posts', () => {
+      const posts = service.findAll();
+      expect(posts.length).toBe(20);
+    });
+  });
+});
