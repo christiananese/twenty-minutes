cbdcfeb6777f7a68bb2bf95a1e317f0486daecdb
Christian Anese <christian.anese@gmail.com>
2020-12-16 13:04:47 +0100

    [API] Test setup for post controller
    Add a test for findAll and only stub for findOne.

--- api/src/modules/posts/posts.service.spec.ts
+++ api/src/modules/posts/posts.service.spec.ts
@@ -2,24 +2,43 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
-  let service: PostsService;
+  let postService: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PostsService],
     }).compile();
 
-    service = module.get<PostsService>(PostsService);
+    postService = module.get<PostsService>(PostsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  describe('when initialized', () => {
-    it('should create 20 dummy posts', () => {
-      const posts = service.findAll();
+  describe('findAll posts', () => {
+    it('should create 20 dummy posts by default', () => {
+      const posts = postService.findAll();
       expect(posts.length).toBe(20);
+    });
+
+    it('should get all posts', async () => {
+      const posts = await postService.findAll();
+      jest.spyOn(postService, 'findAll').mockImplementation(() => posts);
+      expect(posts.length).toBe(20);
+    });
+
+    it('should return an empty array if there are no posts', async () => {
+      jest.spyOn(postService, 'findAll').mockImplementation(() => []);
+      const posts = await postService.findAll();
+      expect(postService.findAll).toHaveBeenCalled();
+      expect(posts.length).toBe(0);
+    });
+  });
+
+  describe('findOne post', () => {
+    it('should get a post by SLUG', async () => {
+      expect(true).toBe(true);
+    });
+
+    it('throws an error as a post with the provided SLUG is found', async () => {
+      expect(true).toBe(true);
     });
   });
 });
